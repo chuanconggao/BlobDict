20e9ee75c55325f7156ef70369eb16a308103b78
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-03-31 18:27:21 -0700

    Explicitly specify mp3 as format for AudioBlob

--- src/blob_dict/blob/audio.py
+++ src/blob_dict/blob/audio.py
@@ -15,16 +15,21 @@ class AudioData(NamedTuple):
 
 
 class AudioBlob(BytesBlob):
+    __IN_MEMORY_FILE_NAME: str = "file.mp3"
+
     def __init__(self, blob: bytes | AudioData) -> None:
         if isinstance(blob, AudioData):
             bio = BytesIO()
+            bio.name = AudioBlob.__IN_MEMORY_FILE_NAME
             soundfile.write(bio, AudioData.data, AudioData.sample_rate)
             blob = bio.getvalue()
 
         super().__init__(blob)
 
     def as_audio(self) -> AudioData:
-        return AudioData(*soundfile.read(BytesIO(self._blob_bytes)))
+        bio = BytesIO(self._blob_bytes)
+        bio.name = AudioBlob.__IN_MEMORY_FILE_NAME
+        return AudioData(*soundfile.read(bio))
 
     @override
     def __repr__(self) -> str:
