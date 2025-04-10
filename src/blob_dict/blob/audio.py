0ed028f1ffbffcd07d35fab6d499708b1ffebdf5
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-04-10 06:12:25 -0700

    Add AudioClip support in AudioBlob

--- src/blob_dict/blob/audio.py
+++ src/blob_dict/blob/audio.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
 from io import BytesIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 from typing import NamedTuple, override
 
 import numpy
 import soundfile
+from moviepy.audio.AudioClip import AudioClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
 
 from . import BytesBlob
 
@@ -17,8 +21,16 @@ class AudioData(NamedTuple):
 class AudioBlob(BytesBlob):
     __IN_MEMORY_FILE_NAME: str = "file.mp3"
 
-    def __init__(self, blob: bytes | AudioData) -> None:
-        if isinstance(blob, AudioData):
+    def __init__(self, blob: bytes | AudioClip | AudioData) -> None:
+        if isinstance(blob, AudioClip):
+            with NamedTemporaryFile(suffix=".mp3", delete_on_close=False) as f:
+                blob.write_audiofile(f.name)
+                blob.close()
+
+                f.close()
+
+                blob = Path(f.name).read_bytes()
+        elif isinstance(blob, AudioData):
             bio = BytesIO()
             bio.name = AudioBlob.__IN_MEMORY_FILE_NAME
             soundfile.write(bio, AudioData.data, AudioData.sample_rate)
@@ -26,7 +38,12 @@ class AudioBlob(BytesBlob):
 
         super().__init__(blob)
 
-    def as_audio(self) -> AudioData:
+    def as_audio(self, filename: str) -> AudioFileClip:
+        Path(filename).write_bytes(self._blob_bytes)
+
+        return AudioFileClip(filename)
+
+    def as_audio_data(self) -> AudioData:
         bio = BytesIO(self._blob_bytes)
         bio.name = AudioBlob.__IN_MEMORY_FILE_NAME
         return AudioData(*soundfile.read(bio))
